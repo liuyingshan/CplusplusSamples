1c3c858cb3034d953088291af63bb20826d06b37
liuyingshan <liuyingshan070505@gmail.com>
2015-03-17 14:09:18 +0800

    enhance char samples

--- CplusplusSamples/charSamples.h
+++ CplusplusSamples/charSamples.h
@@ -1,18 +1,26 @@
-#pragma  warning(disable:4996)
+#pragma  warning(disable:4996) // avoid unsafe function warning
 
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
 class CharSamples {
 public:
 	CharSamples() {
-		printf("--------char samples--------\n");
+		printf("\n--------char samples--------\n");
 
-		/*
-		 object char
-		 */
+		objectChar();
+		pointerChar();
+		arrayChar();
+		char2String();
+		string2char();
+	}
+
+	void objectChar() {
+		printf("\n{object char sample}\n");
+
 		char c1[] = "abc";
 		char c2[4] = "def";
 		//char c3[3] = "ghi"; // Error: "ghi" needs at least 4 bytes.
@@ -21,17 +29,52 @@ public:
 		printf("%d %d %s\n", &c1, c1, c1);
 		printf("%d %d %s\n", &c2, c2, c2);
 		printf("%d %d %s\n", &c4, c4, c4);
+	}
 
-		/*
-		 pointer to object char
-		 */
+	void pointerChar() {
+		printf("\n{pointer char sample}\n");
+
 		char *p1 = "123";
-		char *p2 = (char *)malloc(10);
+		char *p2 = new char[10];
 		strcpy(p2, "456");
 		// *p1 = '7'; // Error: p1 points to constant "123" which can not be changed.
 		*p2 = '8';
 
-		printf("%s\n", p1);
-		printf("%s\n", p2); // Output: 856
+		printf("%d %d %s\n", &p1, p1, p1);
+		printf("%d %d %s\n", &p2, p2, p2);
+
+		delete [] p2;
 	}
+
+	void arrayChar() {
+		printf("\n{array char sample}\n");
+
+		char *arr[] = { "abc", "def", NULL };
+		for (int i = 0; arr[i]; i++) {
+			for (int j = 0; arr[i][j] != '\0'; j++) {
+				printf("%c", arr[i][j]);
+			}
+			printf("\n");
+		}
+	}
+
+	void char2String() {
+		printf("\n{char to string sample}\n");
+
+		char c[] = "abc";
+		string s(c);
+		printf("%s\n", s.c_str());
+		//printf("%s\n", s); // Error
+	}
+
+	void string2char() {
+		printf("\n{string to char sample}\n");
+
+		string s("abc");
+		char *c = new char[s.length() + 1];
+		strcpy(c, s.c_str());
+		printf("%s\n", c);
+		delete [] c;
+	}
+
 };
